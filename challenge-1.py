dad815f3d1d07e6f6fc229bc534379fa1e9f16a8
trupti lets go <trupti.v19625@gmail.com>
2024-03-24 14:08:00 +0530

    completed day 1

--- challenge-1.py
+++ challenge-1.py
@@ -1,1 +1,4 @@
 print("this is the band name generator program")
+city= input("enter the name of a city: ")
+pet= input("enter the name of your pet: ")
+print("your band name could be {} {}".format(city, pet))
