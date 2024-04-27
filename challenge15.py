ffbc989fad815ce3d9b0faaf4a7da7c5f3857436
trupti lets go <trupti.v19625@gmail.com>
2024-04-27 15:26:46 +0530

    started coffee machine

--- challenge15.py
+++ challenge15.py
@@ -1,2 +1,20 @@
 # day 15- coffee machine project
 
+'''make 3 hot flavours-
+espresso-50  ml water 18g coffee 
+latte 200ml water 24 g coffee 150 ml milk
+cappuccin0 250 ml water 24 g coffee 100 ml milk
+
+coin operated
+print report
+
+check if resources are sufficient
+
+process coins
+
+check transaction is successful?
+
+make coffee- deduct resources
+
+
+'''
