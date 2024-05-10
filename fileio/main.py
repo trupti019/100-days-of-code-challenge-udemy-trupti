0afdede55767115e530ac4ca75ae251e96995dea
trupti lets go <trupti.v19625@gmail.com>
2024-05-10 11:22:19 +0530

    completed file io

--- fileio/main.py
+++ fileio/main.py
@@ -1,1 +1,10 @@
+with open ('names.txt', 'r') as file:
+    list_names= file.readlines()
 
+with open ('letter.txt', 'r') as p:
+    letter_content = p.read()
+    for i in list_names:
+        name= i.strip()
+        new_letter= letter_content.replace('[name]', name)
+        with open (f'{name}_gm.txt', 'w') as f:
+            f.write(new_letter)
